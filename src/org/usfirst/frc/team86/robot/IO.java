03fea7502227cc63d448bff2b5cb59810fd46834
86Kaitlyn <kitkat33633@gmail.com>
2017-12-09 22:25:48 -0500

    kill me

---
+++ src/org/usfirst/frc/team86/robot/IO.java
@@ -0,0 +1,26 @@
+package org.usfirst.frc.team86.robot;
+
+import edu.wpi.first.wpilibj.joystick.Joystick;
+
+public class IO extends OI {
+	
+
+
+
+	public static Victor Shooter = new Victor (0)
+	public static Victor Conveyor = new Victor (1)
+	public static Victor Agitator = new Victor (2)
+	public static Victor Vibrtor = new Victor (3)
+	public static Victor Snorfler = new Victor (4)
+	public static Victor Leftfrontdrive = new Victor (5)
+	public static Victor Left Rear Drive = new Victor (6)
+	public static Victor Right Front Drive = new Victor (7)
+	public static Victor Right Rear Drive = new Victor (8)
+		
+	public static Joystick Left Joystick = new Joystick (0)
+	public static Joystick Right Joystick = new Joystick (1)
+	public static Joystick CO Joystiick = new Joystick (2)
+
+	}
+
+}
